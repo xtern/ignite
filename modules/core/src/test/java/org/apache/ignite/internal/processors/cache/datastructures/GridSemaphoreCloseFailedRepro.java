ad14288090d8f12a1bcb28321593a105075b3420
xtern <30606288+xtern@users.noreply.github.com>
2017-09-28 16:52:02 +0300

    edits

---
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/datastructures/GridSemaphoreCloseFailedRepro.java
@@ -0,0 +1,67 @@
+package org.apache.ignite.internal.processors.cache.datastructures;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteInterruptedException;
+import org.apache.ignite.IgniteSemaphore;
+import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
+
+public class GridSemaphoreCloseFailedRepro extends GridCommonAbstractTest {
+
+    public void testBrokenSemaphore() throws Exception {
+
+        final boolean[] success = new boolean[1];
+
+        try (Ignite node = startGrid(0)) {
+
+            for (; ; ) {
+                final AtomicReference<IgniteSemaphore> ref = new AtomicReference<>();
+
+                success[0] = false;
+
+                Thread t = new Thread(new Runnable() {
+                    @Override public void run() {
+                        IgniteSemaphore semaphore = node.semaphore("test", 1, true, true);
+                        try {
+                            ref.set(semaphore);
+
+                            while (semaphore.availablePermits() > 0)
+                                ;
+
+                            semaphore.acquire(1);
+                        }
+                        catch (IgniteInterruptedException ignore) {
+                            // ignore - normal behavior
+                        }
+                        finally {
+                            semaphore.close();
+                            // test failed
+                            success[0] = true;
+                        }
+                    }
+                });
+
+                t.start();
+
+                IgniteSemaphore semaphore;
+
+                while ((semaphore = ref.get()) == null)
+                    ;
+
+                semaphore.acquire(1);
+
+                while (!semaphore.hasQueuedThreads())
+                    ;
+
+                t.interrupt();
+
+                t.join();
+
+                if (!success[0])
+                    fail("Failed to close semaphore");
+            }
+        }
+    }
+
+}
