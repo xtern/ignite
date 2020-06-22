77be8d65a2b64d861e081ff1b92b491bde6e94b9
Pavel Pereslegin <xxtern@gmail.com>
2020-06-22 16:32:02 +0300

    IGNITE-12843 Code cleanup (wip).

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/GridCacheOffheapManager.java
@@ -1027,6 +1027,8 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
 
                         pageStore.encryptPageCount(encrPageCnt);
                         pageStore.encryptPageIndex(pageIO.getEncryptPageIndex(pageAddr));
+
+                        ctx.kernalContext().encryption().markForReencryption(grpId);
                     }
 
                     System.out.println("init meta " + grpId + " p=" + PageIdAllocator.INDEX_PARTITION + " pagesCnt=" + encrPageCnt);
@@ -2008,8 +2010,11 @@ public class GridCacheOffheapManager extends IgniteCacheOffheapManagerImpl imple
 
                                     pageStore.encryptPageCount(encrPageCnt);
                                     pageStore.encryptPageIndex(io.getEncryptPageIndex(pageAddr));
+
+                                    ctx.kernalContext().encryption().markForReencryption(grpId);
                                 }
 
+                                // todo remove
                                 if (log.isInfoEnabled())
                                     log.info("init meta " + grp.name() + " p=" + partId + " encrPageCnt=" + encrPageCnt);
 
