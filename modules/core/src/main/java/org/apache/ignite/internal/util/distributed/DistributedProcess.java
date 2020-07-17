8c921bb4d3fc3a94aa6b1f4f3bba87d24fe9a675
Pavel Pereslegin <xxtern@gmail.com>
2020-07-17 10:05:50 +0300

    IGNITE-12843 No need for all blt nodes to present during rotation.

--- modules/core/src/main/java/org/apache/ignite/internal/util/distributed/DistributedProcess.java
+++ modules/core/src/main/java/org/apache/ignite/internal/util/distributed/DistributedProcess.java
@@ -97,7 +97,7 @@ public class DistributedProcess<I extends Serializable, R extends Serializable> 
         Function<I, IgniteInternalFuture<R>> exec,
         CI3<UUID, Map<UUID, R>, Map<UUID, Exception>> finish
     ) {
-        this(ctx, type, exec, finish, (id, req) -> new InitMessage<>(id, type, req), false);
+        this(ctx, type, exec, finish, (id, req) -> new InitMessage<>(id, type, req));
     }
 
     /**
@@ -112,8 +112,7 @@ public class DistributedProcess<I extends Serializable, R extends Serializable> 
         DistributedProcessType type,
         Function<I, IgniteInternalFuture<R>> exec,
         CI3<UUID, Map<UUID, R>, Map<UUID, Exception>> finish,
-        BiFunction<UUID, I, ? extends InitMessage<I>> initMsgFactory,
-        boolean failOnNodeLeftOrFail
+        BiFunction<UUID, I, ? extends InitMessage<I>> initMsgFactory
     ) {
         this.ctx = ctx;
         this.type = type;
@@ -199,15 +198,7 @@ public class DistributedProcess<I extends Serializable, R extends Serializable> 
 
             for (Process p : processes.values()) {
                 p.initFut.listen(fut -> {
-                    boolean crdLeft = F.eq(leftNodeId, p.crdId);
-
-                    if (failOnNodeLeftOrFail && (crdLeft || p.remaining.contains(leftNodeId))) {
-                        p.singleMsgs.put(leftNodeId, new SingleNodeMessage<>(p.id, type, null,
-                            new IgniteCheckedException("The node left the cluster before the process was completed " +
-                                "[node=" + leftNodeId + ", processId=" + p.id + "]")));
-                    }
-
-                    if (crdLeft) {
+                    if (F.eq(leftNodeId, p.crdId)) {
                         ClusterNode crd = coordinator();
 
                         if (crd == null) {
@@ -234,6 +225,8 @@ public class DistributedProcess<I extends Serializable, R extends Serializable> 
                         }
 
                         if (rmvd) {
+                            assert !p.singleMsgs.containsKey(leftNodeId);
+
                             if (isEmpty)
                                 finishProcess(p);
                         }
