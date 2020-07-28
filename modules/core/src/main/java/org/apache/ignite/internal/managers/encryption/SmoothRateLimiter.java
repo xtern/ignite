47e7aeb18fbf40d047548dc42cc419bcbdf394ff
Pavel Pereslegin <xxtern@gmail.com>
2020-07-28 12:37:36 +0300

    wip

--- modules/core/src/main/java/org/apache/ignite/internal/managers/encryption/SmoothRateLimiter.java
+++ modules/core/src/main/java/org/apache/ignite/internal/managers/encryption/SmoothRateLimiter.java
@@ -19,6 +19,8 @@ package org.apache.ignite.internal.managers.encryption;
 
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+import org.apache.ignite.internal.IgniteInterruptedCheckedException;
+import org.apache.ignite.internal.util.typedef.internal.U;
 import org.jetbrains.annotations.Nullable;
 
 import static java.lang.Math.max;
@@ -27,34 +29,57 @@ import static java.util.concurrent.TimeUnit.MICROSECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-abstract class SmoothRateLimiter {
+public class SmoothRateLimiter {
     /**
      * The underlying timer; used both to measure elapsed time and sleep as necessary. A separate
      * object to facilitate testing.
      */
-    private final SleepingStopwatch stopwatch;
+    private final Stopwatch stopwatch;
 
+    /** Mutex. */
     private final Object mux = new Object();
 
-    public static SmoothRateLimiter create(double permitsPerSecond, long warmupPeriod, TimeUnit unit) {
-        checkArgument(warmupPeriod >= 0, "warmupPeriod must not be negative: %s", warmupPeriod);
-        return create(permitsPerSecond, warmupPeriod, unit, 3.0, new SleepingStopwatch());
-    }
+    /** Warmup period in microsecods. */
+    private final long warmupPeriodMicros;
+
+    /**
+     * The slope of the line from the stable interval (when permits == 0), to the cold interval
+     * (when permits == maxPermits)
+     */
+    private double slope;
+
+    private double thresholdPermits;
+    private double coldFactor;
+
+    /** The currently stored permits. */
+    private double storedPermits;
+
+    /** The maximum number of stored permits. */
+    private double maxPermits;
+
+    /**
+     * The interval between two unit requests, at our stable rate. E.g., a stable rate of 5 permits
+     * per second has a stable interval of 200ms.
+     */
+    private double stableIntervalMicros;
+
+    /**
+     * The time when the next request (no matter its size) will be granted. After granting a request,
+     * this is pushed further in the future. Large requests push this further than small requests.
+     */
+    private long nextFreeTicketMicros = 0L; // could be either in the past or future
 
     public static void checkArgument(boolean flag, @Nullable String errorMessageTemplate, Object ... objs) {
         if (!flag)
             throw new IllegalArgumentException(String.format(errorMessageTemplate, objs));
     }
 
-    static SmoothRateLimiter create(
-        double permitsPerSecond,
-        long warmupPeriod,
-        TimeUnit unit,
-        double coldFactor,
-        SleepingStopwatch stopwatch) {
-        SmoothRateLimiter rateLimiter = new SmoothWarmingUp(stopwatch, warmupPeriod, unit, coldFactor);
-        rateLimiter.setRate(permitsPerSecond);
-        return rateLimiter;
+    public SmoothRateLimiter(double permitsPerSecond, long warmupPeriod, TimeUnit timeUnit, double coldFactor) {
+        this.stopwatch = new Stopwatch();
+        this.coldFactor = coldFactor;
+        this.warmupPeriodMicros = timeUnit.toMicros(warmupPeriod);
+
+        setRate(permitsPerSecond);
     }
 
     private static void checkPermits(int permits) {
@@ -96,7 +121,7 @@ abstract class SmoothRateLimiter {
      */
     public final double getRate() {
         synchronized (mux) {
-            return doGetRate();
+            return SECONDS.toMicros(1L) / stableIntervalMicros;
         }
     }
 
@@ -109,10 +134,17 @@ abstract class SmoothRateLimiter {
      * @throws IllegalArgumentException if the requested number of permits is negative or zero
      * @since 16.0 (present in 13.0 with {@code void} return type})
      */
-    public double acquire(int permits) {
+    public double acquire(int permits) throws IgniteInterruptedCheckedException {
         long microsToWait = reserve(permits);
 
-        stopwatch.sleepMicrosUninterruptibly(microsToWait);
+        try {
+            MICROSECONDS.sleep(microsToWait);
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+
+            throw new IgniteInterruptedCheckedException(e);
+        }
 
         return 1.0 * microsToWait / SECONDS.toMicros(1L);
     }
@@ -135,138 +167,56 @@ abstract class SmoothRateLimiter {
         }
     }
 
-    @Override
-    public String toString() {
-        return String.format(Locale.ROOT, "RateLimiter[stableRate=%3.1fqps]", getRate());
-    }
-
-    static final class SmoothWarmingUp extends SmoothRateLimiter {
-        private final long warmupPeriodMicros;
-        /**
-         * The slope of the line from the stable interval (when permits == 0), to the cold interval
-         * (when permits == maxPermits)
-         */
-        private double slope;
-
-        private double thresholdPermits;
-        private double coldFactor;
-
-        SmoothWarmingUp(
-            SmoothRateLimiter.SleepingStopwatch stopwatch, long warmupPeriod, TimeUnit timeUnit, double coldFactor) {
-            super(stopwatch);
-            this.warmupPeriodMicros = timeUnit.toMicros(warmupPeriod);
-            this.coldFactor = coldFactor;
-        }
-
-        @Override
-        void doSetRate(double permitsPerSecond, double stableIntervalMicros) {
-            double oldMaxPermits = maxPermits;
-            double coldIntervalMicros = stableIntervalMicros * coldFactor;
-            thresholdPermits = 0.5 * warmupPeriodMicros / stableIntervalMicros;
-            maxPermits =
-                thresholdPermits + 2.0 * warmupPeriodMicros / (stableIntervalMicros + coldIntervalMicros);
-            slope = (coldIntervalMicros - stableIntervalMicros) / (maxPermits - thresholdPermits);
-            if (oldMaxPermits == Double.POSITIVE_INFINITY) {
-                // if we don't special-case this, we would get storedPermits == NaN, below
-                storedPermits = 0.0;
-            } else {
-                storedPermits =
-                    (oldMaxPermits == 0.0)
-                        ? maxPermits // initial state is cold
-                        : storedPermits * maxPermits / oldMaxPermits;
-            }
-        }
-
-        @Override
-        long storedPermitsToWaitTime(double storedPermits, double permitsToTake) {
-            double availablePermitsAboveThreshold = storedPermits - thresholdPermits;
-            long micros = 0;
-            // measuring the integral on the right part of the function (the climbing line)
-            if (availablePermitsAboveThreshold > 0.0) {
-                double permitsAboveThresholdToTake = min(availablePermitsAboveThreshold, permitsToTake);
-                // TODO(cpovirk): Figure out a good name for this variable.
-                double length =
-                    permitsToTime(availablePermitsAboveThreshold)
-                        + permitsToTime(availablePermitsAboveThreshold - permitsAboveThresholdToTake);
-                micros = (long) (permitsAboveThresholdToTake * length / 2.0);
-                permitsToTake -= permitsAboveThresholdToTake;
-            }
-            // measuring the integral on the left part of the function (the horizontal line)
-            micros += (long) (stableIntervalMicros * permitsToTake);
-            return micros;
-        }
-
-        private double permitsToTime(double permits) {
-            return stableIntervalMicros + permits * slope;
-        }
-
-        @Override double coolDownIntervalMicros() {
-            return warmupPeriodMicros / maxPermits;
-        }
-    }
-
-    /** The currently stored permits. */
-    double storedPermits;
-
-    /** The maximum number of stored permits. */
-    double maxPermits;
-
-    /**
-     * The interval between two unit requests, at our stable rate. E.g., a stable rate of 5 permits
-     * per second has a stable interval of 200ms.
-     */
-    double stableIntervalMicros;
-
-    /**
-     * The time when the next request (no matter its size) will be granted. After granting a request,
-     * this is pushed further in the future. Large requests push this further than small requests.
-     */
-    private long nextFreeTicketMicros = 0L; // could be either in the past or future
-
-    private SmoothRateLimiter(SleepingStopwatch stopwatch) {
-        assert stopwatch != null;
-
-        this.stopwatch = stopwatch;
-    }
-
     final void doSetRate(double permitsPerSecond, long nowMicros) {
         resync(nowMicros);
         double stableIntervalMicros = SECONDS.toMicros(1L) / permitsPerSecond;
         this.stableIntervalMicros = stableIntervalMicros;
-        doSetRate(permitsPerSecond, stableIntervalMicros);
+
+        double oldMaxPermits = maxPermits;
+        double coldIntervalMicros = stableIntervalMicros * coldFactor;
+        thresholdPermits = 0.5 * warmupPeriodMicros / stableIntervalMicros;
+        maxPermits =
+            thresholdPermits + 2.0 * warmupPeriodMicros / (stableIntervalMicros + coldIntervalMicros);
+        slope = (coldIntervalMicros - stableIntervalMicros) / (maxPermits - thresholdPermits);
+        if (oldMaxPermits == Double.POSITIVE_INFINITY) {
+            // if we don't special-case this, we would get storedPermits == NaN, below
+            storedPermits = 0.0;
+        } else {
+            storedPermits =
+                (oldMaxPermits == 0.0)
+                    ? maxPermits // initial state is cold
+                    : storedPermits * maxPermits / oldMaxPermits;
+        }
     }
 
-    abstract void doSetRate(double permitsPerSecond, double stableIntervalMicros);
-
-    final double doGetRate() {
-        return SECONDS.toMicros(1L) / stableIntervalMicros;
-    }
-
-    final long queryEarliestAvailable(long nowMicros) {
-        return nextFreeTicketMicros;
-    }
-
-    final long reserveEarliestAvailable(int requiredPermits, long nowMicros) {
+    private long reserveEarliestAvailable(int requiredPermits, long nowMicros) {
         resync(nowMicros);
-        long returnValue = nextFreeTicketMicros;
+
+        long res = nextFreeTicketMicros;
+
         double storedPermitsToSpend = min(requiredPermits, this.storedPermits);
+
         double freshPermits = requiredPermits - storedPermitsToSpend;
+
         long waitMicros =
             storedPermitsToWaitTime(this.storedPermits, storedPermitsToSpend)
                 + (long) (freshPermits * stableIntervalMicros);
 
         this.nextFreeTicketMicros = saturatedAdd(nextFreeTicketMicros, waitMicros);
         this.storedPermits -= storedPermitsToSpend;
-        return returnValue;
+
+        return res;
     }
 
     private static long saturatedAdd(long a, long b) {
         long naiveSum = a + b;
+
         if ((a ^ b) < 0 | (a ^ naiveSum) >= 0) {
             // If a and b have different signs or a has the same sign as the result then there was no
             // overflow, return.
             return naiveSum;
         }
+
         // we did over/under flow, if the sign is negative we should return MAX otherwise MIN
         return Long.MAX_VALUE + ((naiveSum >>> (Long.SIZE - 1)) ^ 1);
     }
@@ -278,81 +228,66 @@ abstract class SmoothRateLimiter {
      *
      * <p>This always holds: {@code 0 <= permitsToTake <= storedPermits}
      */
-    abstract long storedPermitsToWaitTime(double storedPermits, double permitsToTake);
+    long storedPermitsToWaitTime(double storedPermits, double permitsToTake) {
+        double availablePermitsAboveThreshold = storedPermits - thresholdPermits;
+        long micros = 0;
+        // measuring the integral on the right part of the function (the climbing line)
+        if (availablePermitsAboveThreshold > 0.0) {
+            double permitsAboveThresholdToTake = min(availablePermitsAboveThreshold, permitsToTake);
+            // TODO(cpovirk): Figure out a good name for this variable.
+            double length =
+                permitsToTime(availablePermitsAboveThreshold)
+                    + permitsToTime(availablePermitsAboveThreshold - permitsAboveThresholdToTake);
+            micros = (long) (permitsAboveThresholdToTake * length / 2.0);
+            permitsToTake -= permitsAboveThresholdToTake;
+        }
+        // measuring the integral on the left part of the function (the horizontal line)
+        micros += (long) (stableIntervalMicros * permitsToTake);
+        return micros;
+    }
+
+    private double permitsToTime(double permits) {
+        return stableIntervalMicros + permits * slope;
+    }
 
     /**
      * Returns the number of microseconds during cool down that we have to wait to get a new permit.
      */
-    abstract double coolDownIntervalMicros();
+    double coolDownIntervalMicros() {
+        return warmupPeriodMicros / maxPermits;
+    }
 
     /** Updates {@code storedPermits} and {@code nextFreeTicketMicros} based on the current time. */
     void resync(long nowMicros) {
         // if nextFreeTicket is in the past, resync to now
         if (nowMicros > nextFreeTicketMicros) {
             double newPermits = (nowMicros - nextFreeTicketMicros) / coolDownIntervalMicros();
+
             storedPermits = min(maxPermits, storedPermits + newPermits);
+
             nextFreeTicketMicros = nowMicros;
         }
     }
 
-    private static class SleepingStopwatch {
+    @Override public String toString() {
+        return String.format(Locale.ROOT, "RateLimiter[stableRate=%3.1fqps]", getRate());
+    }
+
+    private static class Stopwatch {
+        private final long startTick = System.nanoTime();
+
         /*
          * We always hold the mutex when calling this. TODO(cpovirk): Is that important? Perhaps we need
          * to guarantee that each call to reserveEarliestAvailable, etc. sees a value >= the previous?
          * Also, is it OK that we don't hold the mutex when sleeping?
          */
         protected long readMicros() {
-            return MICROSECONDS.convert(elapsedNanos(), NANOSECONDS);
-        }
-
-        private final long startTick;
-
-        /**
-         * Starts the stopwatch.
-         *
-         * @return this {@code Stopwatch} instance
-         * @throws IllegalStateException if the stopwatch is already running.
-         */
-        public SleepingStopwatch() {
-            startTick = System.nanoTime();
-        }
-
-        private long elapsedNanos() {
-            return System.nanoTime() - startTick;
-        }
-
-        protected void sleepMicrosUninterruptibly(long micros) {
-            if (micros <= 0)
-                return;
-
-            boolean interrupted = false;
-
-            try {
-                long remainingNanos = MICROSECONDS.toNanos(micros);
-
-                long end = System.nanoTime() + remainingNanos;
-
-                while (true) {
-                    try {
-                        // TimeUnit.sleep() treats negative timeouts just like zero.
-                        NANOSECONDS.sleep(remainingNanos);
-
-                        return;
-                    } catch (InterruptedException e) {
-                        interrupted = true;
-
-                        remainingNanos = end - System.nanoTime();
-                    }
-                }
-            } finally {
-                if (interrupted)
-                    Thread.currentThread().interrupt();
-            }
+            return MICROSECONDS.convert(System.nanoTime() - startTick, NANOSECONDS);
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        SmoothRateLimiter limiter = create(10, 1, TimeUnit.SECONDS);
+    public static void main(String[] args) throws IgniteInterruptedCheckedException {
+        SmoothRateLimiter limiter = new SmoothRateLimiter(2, 1, TimeUnit.SECONDS, 3.0);
 
         for (int i = 0; i < 100; i++) {
             limiter.acquire(1);
@@ -360,7 +295,9 @@ abstract class SmoothRateLimiter {
             System.out.println(">>> " + i);
 
             if (i == 50)
-                Thread.sleep(2_000);
+                U.sleep(2_000);
         }
     }
+
+
 }
