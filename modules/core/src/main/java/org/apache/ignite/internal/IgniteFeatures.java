5df33aeb906696b79732fa19a67209db94605a5a
Pavel Pereslegin <xxtern@gmail.com>
2020-08-03 14:35:54 +0300

    IGNITE-12843 Fix after merge.

--- modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
+++ modules/core/src/main/java/org/apache/ignite/internal/IgniteFeatures.java
@@ -122,7 +122,7 @@ public enum IgniteFeatures {
     INDEXES_MANIPULATIONS_FROM_CONTROL_SCRIPT(42),
 
     /** Cache encryption key change. See {@link IgniteEncryption#changeCacheGroupKey(Collection)}. */
-    CACHE_GROUP_KEY_CHANGE(41);
+    CACHE_GROUP_KEY_CHANGE(43);
 
     /**
      * Unique feature identifier.
